0b35fb56cfc59b02a02866e61a0dd48010624d6b
NwaguChima <nwaguchima48@gmail.com>
2022-08-16 19:32:51 +0100

    imported layouts

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import Login from "./components/Login";
 import Public from "./components/Public";
+import DashLayout from "./components/DashLayout";
 
 function App() {
   return (
@@ -9,6 +10,8 @@ function App() {
       <Route path="/" element={<Layout />}>
         <Route index element={<Public />} />
         <Route path="login" element={<Login />} />
+
+        <Route path="dash" element={<DashLayout />}></Route>
       </Route>
     </Routes>
   );
