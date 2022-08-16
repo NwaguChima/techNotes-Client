90bbd720316b59067cae403bfa653940a84103ea
NwaguChima <nwaguchima48@gmail.com>
2022-08-16 19:33:46 +0100

    setup auth dir

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
-import Login from "./components/Login";
+import Login from "./features/auth/Login";
 import Public from "./components/Public";
 import DashLayout from "./components/DashLayout";
 
