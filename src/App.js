93279841244ba5f49e4903b12794f0c18c0bc3bb
NwaguChima <nwaguchima48@gmail.com>
2022-08-16 19:07:21 +0100

    routed for login

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
+import Login from "./components/Login";
 import Public from "./components/Public";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Public />} />
+        <Route path="login" element={<Login />} />
       </Route>
     </Routes>
   );
