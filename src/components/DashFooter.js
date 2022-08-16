40ec8da857eafb670831407cb84ce05fa3f97c95
NwaguChima <nwaguchima48@gmail.com>
2022-08-16 19:31:13 +0100

    setup navigation with gohomebutton

--- src/components/DashFooter.js
+++ src/components/DashFooter.js
@@ -4,8 +4,28 @@ import { faHouse } from "@fortawesome/free-solid-svg-icons";
 import { useNavigate, useLocation } from "react-router-dom";
 
 const DashFooter = () => {
+  const navigate = useNavigate();
+  const { pathName } = useLocation();
+
+  const onGoHomeClicked = () => navigate("/dash");
+
+  let goHomeButton = null;
+
+  if (pathName !== "/dash") {
+    goHomeButton = (
+      <button
+        className="dash-footer__button icon-button"
+        title="Home"
+        onClick={onGoHomeClicked}
+      >
+        <FontAwesomeIcon icon={faHouse} />
+      </button>
+    );
+  }
+
   const content = (
     <footer className="dash-footer">
+      {goHomeButton}
       <p>Current User:</p>
       <p>Status:</p>
     </footer>
