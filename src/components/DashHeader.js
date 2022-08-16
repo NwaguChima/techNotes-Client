583189150d22f4f568714e052ecf5c915909d3c0
NwaguChima <nwaguchima48@gmail.com>
2022-08-16 19:20:12 +0100

    setu dash header content

--- src/components/DashHeader.js
+++ src/components/DashHeader.js
@@ -2,7 +2,18 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const DashHeader = () => {
-  return <div>DashHeader</div>;
+  const content = (
+    <header className="dash-header">
+      <div className="dash-header__container">
+        <Link to="/dash/notes">
+          <h1 className="dash-header__title">techNotes</h1>
+        </Link>
+        <nav className="dash-header__nav">{}</nav>
+      </div>
+    </header>
+  );
+
+  return content;
 };
 
 export default DashHeader;
