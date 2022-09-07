073d1fb42f4bc7ee424a56723f4124bcd812654e
NwaguChima <nwaguchima48@gmail.com>
2022-09-07 02:39:03 +0100

    setup content

--- src/features/users/NewUserForm.js
+++ src/features/users/NewUserForm.js
@@ -73,12 +73,68 @@ const NewUserForm = () => {
 
   const errClass = isError ? "errmsg" : "offscreen";
   const validUserClass = !validUsername ? "form__input--incomplete" : "";
-  const valiePwdClass = !validPassword ? "form__input--incomplete" : "";
+  const validPwdClass = !validPassword ? "form__input--incomplete" : "";
   const validRolesClass = !Boolean(roles.length)
     ? "form__input--incomplete"
     : "";
 
-  return <div>NewUserForm</div>;
+  const content = (
+    <>
+      <p className={errClass}>{error?.data?.message}</p>
+
+      <form className="form" onSubmit={onSaveUserClicked}>
+        <div className="form__title-row">
+          <h2>New User</h2>
+          <div className="form__action-buttons">
+            <button className="icon-button" title="Save" disabled={!canSave}>
+              <FontAwesomeIcon icon={faSave} />
+            </button>
+          </div>
+        </div>
+        <label className="form__label" htmlFor="username">
+          Username: <span className="nowrap">[3-20 letters]</span>
+        </label>
+        <input
+          className={`form__input ${validUserClass}`}
+          id="username"
+          name="username"
+          type="text"
+          autoComplete="off"
+          value={username}
+          onChange={onUsernameChanged}
+        />
+
+        <label className="form__label" htmlFor="password">
+          Password: <span className="nowrap">[4-12 chars incl. !@#$%]</span>
+        </label>
+        <input
+          className={`form__input ${validPwdClass}`}
+          id="password"
+          name="password"
+          type="password"
+          value={password}
+          onChange={onPasswordChanged}
+        />
+
+        <label className="form__label" htmlFor="roles">
+          ASSIGNED ROLES:
+        </label>
+        <select
+          id="roles"
+          name="roles"
+          className={`form__select ${validRolesClass}`}
+          multiple={true}
+          size="3"
+          value={roles}
+          onChange={onRolesChanged}
+        >
+          {options}
+        </select>
+      </form>
+    </>
+  );
+
+  return content;
 };
 
 export default NewUserForm;
