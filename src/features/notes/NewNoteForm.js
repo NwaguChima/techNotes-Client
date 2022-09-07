f9eb528e3c045da2857ba6da892b606e5f15e8b5
NwaguChima <nwaguchima48@gmail.com>
2022-09-07 03:24:03 +0100

    inports for new notes form

--- src/features/notes/NewNoteForm.js
+++ src/features/notes/NewNoteForm.js
@@ -1,4 +1,8 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAddNewNoteMutation } from "./notesApiSlice";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSave } from "@fortawesome/free-solid-svg-icons";
 
 const NewNoteForm = () => {
   return <div>NewNoteForm</div>;
