7657ad5fc606568849b094a74c12b8da825074e2
NwaguChima <nwaguchima48@gmail.com>
2022-09-07 03:21:59 +0100

    setup states for edit note form

--- src/features/notes/EditNoteForm.js
+++ src/features/notes/EditNoteForm.js
@@ -4,7 +4,21 @@ import { useNavigate } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSave, faTrashCan } from "@fortawesome/free-solid-svg-icons";
 
-const EditNoteForm = () => {
+const EditNoteForm = ({ note, users }) => {
+  const [updateNote, { isLoading, isSuccess, isError, error }] =
+    useUpdateNoteMutation();
+
+  const [
+    deleteNote,
+    { isSuccess: isDelSuccess, isError: isDelError, error: delerror },
+  ] = useDeleteNoteMutation();
+
+  const navigate = useNavigate();
+
+  const [title, setTitle] = useState(note.title);
+  const [text, setText] = useState(note.text);
+  const [completed, setCompleted] = useState(note.completed);
+  const [userId, setUserId] = useState(note.user);
   return <div>EditNoteForm</div>;
 };
 
