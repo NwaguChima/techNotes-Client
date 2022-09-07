cdd48938e997f91b7b84bfda510d42667b44763e
NwaguChima <nwaguchima48@gmail.com>
2022-09-07 03:25:47 +0100

    returned content for new note form

--- src/features/notes/NewNoteForm.js
+++ src/features/notes/NewNoteForm.js
@@ -48,7 +48,64 @@ const NewNoteForm = ({ users }) => {
   const errClass = isError ? "errmsg" : "offscreen";
   const validTitleClass = !title ? "form__input--incomplete" : "";
   const validTextClass = !text ? "form__input--incomplete" : "";
-  return <div>NewNoteForm</div>;
+
+  const content = (
+    <>
+      <p className={errClass}>{error?.data?.message}</p>
+
+      <form className="form" onSubmit={onSaveNoteClicked}>
+        <div className="form__title-row">
+          <h2>New Note</h2>
+          <div className="form__action-buttons">
+            <button className="icon-button" title="Save" disabled={!canSave}>
+              <FontAwesomeIcon icon={faSave} />
+            </button>
+          </div>
+        </div>
+        <label className="form__label" htmlFor="title">
+          Title:
+        </label>
+        <input
+          className={`form__input ${validTitleClass}`}
+          id="title"
+          name="title"
+          type="text"
+          autoComplete="off"
+          value={title}
+          onChange={onTitleChanged}
+        />
+
+        <label className="form__label" htmlFor="text">
+          Text:
+        </label>
+        <textarea
+          className={`form__input form__input--text ${validTextClass}`}
+          id="text"
+          name="text"
+          value={text}
+          onChange={onTextChanged}
+        />
+
+        <label
+          className="form__label form__checkbox-container"
+          htmlFor="username"
+        >
+          ASSIGNED TO:
+        </label>
+        <select
+          id="username"
+          name="username"
+          className="form__select"
+          value={userId}
+          onChange={onUserIdChanged}
+        >
+          {options}
+        </select>
+      </form>
+    </>
+  );
+
+  return content;
 };
 
 export default NewNoteForm;
