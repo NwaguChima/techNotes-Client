35dec4ffa48db5060a4fe5ddb3a3db2435648606
NwaguChima <nwaguchima48@gmail.com>
2022-09-07 03:24:42 +0100

    setup state and useeffects

--- src/features/notes/NewNoteForm.js
+++ src/features/notes/NewNoteForm.js
@@ -4,7 +4,24 @@ import { useAddNewNoteMutation } from "./notesApiSlice";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSave } from "@fortawesome/free-solid-svg-icons";
 
-const NewNoteForm = () => {
+const NewNoteForm = ({ users }) => {
+  const [addNewNote, { isLoading, isSuccess, isError, error }] =
+    useAddNewNoteMutation();
+
+  const navigate = useNavigate();
+
+  const [title, setTitle] = useState("");
+  const [text, setText] = useState("");
+  const [userId, setUserId] = useState(users[0].id);
+
+  useEffect(() => {
+    if (isSuccess) {
+      setTitle("");
+      setText("");
+      setUserId("");
+      navigate("/dash/notes");
+    }
+  }, [isSuccess, navigate]);
   return <div>NewNoteForm</div>;
 };
 
