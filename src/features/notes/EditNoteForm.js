2f0a47b118f28e5f9de1b3ec1cff41efa85cac0d
NwaguChima <nwaguchima48@gmail.com>
2022-09-07 03:21:19 +0100

    imports for edit note form

--- src/features/notes/EditNoteForm.js
+++ src/features/notes/EditNoteForm.js
@@ -1,4 +1,8 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import { useUpdateNoteMutation, useDeleteNoteMutation } from "./notesApiSlice";
+import { useNavigate } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSave, faTrashCan } from "@fortawesome/free-solid-svg-icons";
 
 const EditNoteForm = () => {
   return <div>EditNoteForm</div>;
