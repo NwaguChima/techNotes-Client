19252b6ad0fad3e5eb47631c7ab9c25422e8a83e
NwaguChima <nwaguchima48@gmail.com>
2022-09-07 02:25:31 +0100

    added regex and imports for new user

--- src/features/users/NewUserForm.js
+++ src/features/users/NewUserForm.js
@@ -1,4 +1,12 @@
 import React from "react";
+import { useAddNewUserMutation } from "./usersApiSlice";
+import { useNavigate } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSave } from "@fortawesome/free-solid-svg-icons";
+import { ROLES } from "../../config/roles";
+
+const USER_REGEX = /^[A-z]{3,20}$/;
+const PASSWORD_REGEX = /^[A-z0-9!@#$%]{4,12}$/;
 
 const NewUserForm = () => {
   return <div>NewUserForm</div>;
