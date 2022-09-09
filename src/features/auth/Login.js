9090255a481f1c30d26356c3e7713c07957d0fb3
NwaguChima <nwaguchima48@gmail.com>
2022-09-09 14:25:51 +0100

    setup form content

--- src/features/auth/Login.js
+++ src/features/auth/Login.js
@@ -22,9 +22,43 @@ const Login = () => {
 
   if (isLoading) return <p>Loading...</p>;
 
-  const content = ()
+  const content = (
+    <section className="public">
+      <header>
+        <h1>Employee Login</h1>
+      </header>
+      <main className="login">
+        <form className="form" onsubmit={handleSubmit}>
+          <label htmlFor="username">Username:</label>
+          <input
+            className="form__input"
+            type="text"
+            id="username"
+            ref={userRef}
+            value={username}
+            onChange={handleUserInput}
+            autoComplete="off"
+            required
+          />
 
-  return content ;
+          <label htmlFor="password">Password:</label>
+          <input
+            className="form__input"
+            type="password"
+            id="password"
+            onChange={handlePwdInput}
+            value={password}
+            required
+          />
+        </form>
+      </main>
+      <footer>
+        <Link to="/">Back to Home</Link>
+      </footer>
+    </section>
+  );
+
+  return content;
 };
 
 export default Login;
