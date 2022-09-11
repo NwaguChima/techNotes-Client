c20bde241095a689ce76df24a610e2ded0f516dc
NwaguChima <nwaguchima48@gmail.com>
2022-09-11 01:14:30 +0100

    updated note comp

--- src/features/notes/Note.js
+++ src/features/notes/Note.js
@@ -2,11 +2,14 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPenToSquare } from "@fortawesome/free-solid-svg-icons";
 import { useNavigate } from "react-router-dom";
 
-import { useSelector } from "react-redux";
-import { selectNoteById } from "./notesApiSlice";
+import { useGetNotesQuery } from "./notesApiSlice";
 
 const Note = ({ noteId }) => {
-  const note = useSelector((state) => selectNoteById(state, noteId));
+  const { note } = useGetNotesQuery("notesList", {
+    selectFromResult: ({ data }) => ({
+      note: data?.entites[noteId],
+    }),
+  });
 
   const navigate = useNavigate();
 
