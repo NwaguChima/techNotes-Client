38b17e678fe5ed4dbd43ee1c1af6a61de0121412
NwaguChima <nwaguchima48@gmail.com>
2022-09-11 01:17:13 +0100

    memoized user and updateed query

--- src/features/notes/Note.js
+++ src/features/notes/Note.js
@@ -1,7 +1,7 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPenToSquare } from "@fortawesome/free-solid-svg-icons";
 import { useNavigate } from "react-router-dom";
-
+import { memo } from "react";
 import { useGetNotesQuery } from "./notesApiSlice";
 
 const Note = ({ noteId }) => {
@@ -49,4 +49,7 @@ const Note = ({ noteId }) => {
     );
   } else return null;
 };
-export default Note;
+
+const memoizedNote = memo(Note);
+
+export default memoizedNote;
